91cddc2defbded413f8c7b7a362fabe1dd6ddbbf
George Bollas <george.bollas@gmail.com>
2019-01-23 18:42:39 +1100

    Create Library

--- Noter.WebUI.React/ClientApp/src/components/Libraries/CreateLibrary.js
+++ Noter.WebUI.React/ClientApp/src/components/Libraries/CreateLibrary.js
@@ -11,14 +11,22 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-const LibraryDialog = ({ library, onSubmit, onClose, isOpen }) => {
+const CreateLibrary = ({ library, onClose, isOpen }) => {
     return (
         <Dialog onClose={onClose} open={isOpen}> 
             <DialogTitle id="form-dialog-title">{library.libraryId ? 'Edit ' + library.name : 'Add a new library'}</DialogTitle>
             <DialogContent>
                 <Formik
                     initialValues={library /** {id, name, tags } */}
-                    onSubmit={onSubmit}
+                    onSubmit={e => {
+                        e.preventDefault()
+                        if (!input.value.trim()) {
+                            return
+                        }
+                        dispatch(createLibrary(input.value))
+                        input.value = ''
+                        // close
+                    }}
                     render={({ errors, status, touched, isSubmitting }) => (
                         <Form>
                             <Field type="text" name="name" />
@@ -40,6 +48,36 @@ const LibraryDialog = ({ library, onSubmit, onClose, isOpen }) => {
     );
 };
 
-export default LibraryDialog;
+export default CreateLibrary;
+
+
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions'
+
+const AddTodo = ({ dispatch }) => {
+    let input
+
+    return (
+        <div>
+            <form
+                onSubmit={e => {
+                    e.preventDefault()
+                    if (!input.value.trim()) {
+                        return
+                    }
+                    dispatch(addTodo(input.value))
+                    input.value = ''
+                }}
+            >
+                <input ref={node => (input = node)} />
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default connect()(AddTodo)
 
 
