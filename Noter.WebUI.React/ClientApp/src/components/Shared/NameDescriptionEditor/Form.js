a3eb2375ec248a91748ac52d1d506bcf835eb6f0
George Bollas <george.bollas@gemail.com>
2019-02-27 15:24:27 +1100

    name desc editor

---
+++ Noter.WebUI.React/ClientApp/src/components/Shared/NameDescriptionEditor/Form.js
@@ -0,0 +1,64 @@
+﻿import React from 'react';
+
+import { Formik, Form, Field } from 'formik';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { fieldToTextField, TextField, TextFieldProps } from 'formik-material-ui';
+import { withStyles } from '@material-ui/core/styles';
+
+import { Editor } from 'react-draft-wysiwyg';
+
+import '../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
+import SaveCancel from '../../Shared/SaveCancel'
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    progress: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+    },
+    wrapper: {
+        margin: theme.spacing.unit,
+        padding: theme.spacing.unit,
+        position: 'relative',
+    },
+});
+
+
+//interface Values {
+//    name: string;
+//    tags: string[];
+//}
+
+const form = ({ classes, handleSubmit, handleChange, isRequestedCreateLibrary, handleBlur, values, onSave, errors }) => {
+    return (
+        <Form >
+            <div className={classes.wrapper} >
+                {isRequestedCreateLibrary && <CircularProgress className={classes.progress} />}
+                        <SaveCancel />
+                <div>
+                    <div>
+                        <Field
+                            label="Name"
+                            name="name"
+                            onChange={handleChange} //By default client side validation is done onChange
+                            onBlur={handleBlur} //By default client side validation is also done onBlur
+                            value={values.name}
+                            error={errors.name} //Error display
+                            onEnter={onSave}
+                            component={TextField}
+                        />
+                    </div>
+                    <div>
+                        <Editor />
+                    </div>
+                </div>
+            </div>
+        </Form>);
+}
+
+export default (withStyles(styles, { withTheme: true })(form));
---
+++ Noter.WebUI.React/ClientApp/src/components/Shared/NameDescriptionEditor/index.js
@@ -0,0 +1,119 @@
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
+import { withRouter } from "react-router";
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { withStyles } from '@material-ui/core/styles';
+
+//import TextField from '@material-ui/core/TextField';
+
+import Form from './Form';
+
+import * as actions from '../../../actions/LibraryNavigator';
+
+const createLibrarySchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+});
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    progress: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+    },
+    wrapper: {
+        margin: theme.spacing.unit,
+        padding: theme.spacing.unit,
+        position: 'relative',
+    },
+});
+
+class NameDescriptionEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.formikRef = React.createRef();
+    }
+
+    //const { a } = this.props; -- why cant we do this
+
+    render() {
+        const { onSubmit, isCreateLibraryDialogOpen, classes } = this.props
+
+        return (
+            <Dialog onClose={this.onClose} open={isCreateLibraryDialogOpen} className={classes.root}>
+                <DialogTitle id="form-dialog-title">Add a new library</DialogTitle>
+                <DialogContent>
+                    <Formik ref={this.formikRef}
+                        initialValues={{ name: '', tags: [] }}
+                        onSubmit={this.onClose}
+                        component={Form}
+                        validationSchema={createLibrarySchema}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.onClose} size="small" color="primary">Cancel</Button>
+                    <Button type='submit' onClick={this.onSave} size="small" color="primary">Save</Button>
+                </DialogActions>
+            </Dialog>
+        );
+    };
+
+
+    handleChipChange(chips) {
+        console.debug(chips.lenght);
+        this.formikRef.current.setFieldValue('tags', chips, false);
+    }
+
+    onCreateLibrarySubmit = (name, tags, setSubmitting, setErrors) => {
+
+        var errors = {};
+
+        const { history, createLibraryRequestSuccess, createLibraryDialogOpen, onCreatedSuccessful } = this.props;
+        this.props.requestCreateLibrary(name, tags)
+            .then(result => {
+                setSubmitting(false);
+                createLibraryRequestSuccess(result.data.libraryId); //todo do we need this, should only need if ui changes state or to refresh list
+                createLibraryDialogOpen(false)
+                onCreatedSuccessful(result.data.libraryId);
+                this.props.history.push('/library/' + result.data.libraryId);
+
+            })
+            .catch(e => {
+                setSubmitting(false);
+                setErrors({ name: e.response.data.failures.Name });
+            })
+    }
+
+    onClose = () => {
+        this.props.createLibraryDialogCancel()
+    }
+
+    onSave = () => {
+        this.formikRef.current.submitForm();
+
+    }
+};
+
+export default connect(
+    state => ({
+        isCreateLibraryDialogOpen: state.libraryNavigator.isCreateLibraryDialogOpen
+    }),
+    dispatch => bindActionCreators(actions, dispatch)
+)(withStyles(styles, { withTheme: true })(withRouter((NameDescriptionEditor))));
+
+
