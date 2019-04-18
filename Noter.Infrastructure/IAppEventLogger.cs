abfd2d5f3e0e0d6b6a8dd7d1ab1b53ab09cdf386
George Bollas <george.bollas@gemail.com>
2019-04-18 13:45:18 +1000

    continue to rework App Event/Command Logging

---
+++ Noter.Infrastructure/IAppEventLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Noter.Infrastructure
+{
+    public interface IAppEventLogger
+    {
+        void LogAppError(string message, Exception ex);
+
+        void LogAppStarting();
+
+        void LogAppStopping();
+
+        Task LogCommand(string command, object request, object response);
+
+    }
+}
